a356f3d64f118f19c88df662d19a95a464100664
Sami Männistö <sami.mannisto1@gmail.com>
2024-04-05 13:10:45 +0300

    E.x. 11.17, test branch protections

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
 
-// get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1.07') // change this string to ensure a new version deployed
+  res.send('1.07')
 })
 
 app.get('/health', (req, res) => {
